9cdbc8de6417f9bb18f4f8d66db10142197de246
PilotoDeFusca <ph4581@gmail.com>
2025-01-28 20:55:33 -0300

    {}

--- source/Heuristics_Functions.cpp
+++ source/Heuristics_Functions.cpp
@@ -45,10 +45,12 @@ Solution runILS(vector<Aircraft> aircrafts, Solution solution, int max_iteration
 
 void applyLocalSearch(vector<Aircraft> aircrafts, Solution &solution)
 {
-    vector<pair<function<bool(vector<Aircraft> &, Solution &)>, int>> neighborhoods = {
+    vector<pair<function<bool(vector<Aircraft> &, Solution &)>, int>> neighborhoods = 
+    {
         {shiftNeighborhood, 0},
         {swapNeighborhood, 0},
-        {twoOptNeighborhood, 0}};
+        {twoOptNeighborhood, 0}
+    };
 
     bool improved = true;
 
